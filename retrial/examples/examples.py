28cf80ceb66ab035d55bb5adee492ec9c8e14781
Ankit Chandawala <ankitchandawala@mgail.com>
2015-06-23 13:51:20 +0530

    Adding retrial with timeout parameter

--- retrial/examples/examples.py
+++ retrial/examples/examples.py
@@ -3,17 +3,36 @@ import asyncio
 from ..retrial.retry import retry
 
 
+class ExampleException(Exception):
+    pass
+
+
 def retry_for_result(result):
     return result == 9
 
-def retry_for_exception():
-    return False
 
-@retry(retry_for_result, retry_for_exception)
-def func_to_retry(arg1, arg2):
+def retry_for_exception(exception):
+    return isinstance(exception, ExampleException)
+
+
+@retry(retry_for_result=retry_for_result)
+def check_result(arg1, arg2):
     yield from asyncio.sleep(2)
     return arg1 + arg2
 
-if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(func_to_retry(4, 5))
 
+@retry(retry_for_exception=retry_for_exception)
+def check_exception():
+    yield from asyncio.sleep(2)
+    raise Exception('Dummy exception thrown')
+
+
+@retry(timeout=2)
+def check_timeout():
+    yield from asyncio.sleep(5)
+    return
+
+
+if __name__ == '__main__':
+    asyncio.async(check_timeout())
+    asyncio.get_event_loop().run_forever()
--- retrial/retrial/retry.py
+++ retrial/retrial/retry.py
@@ -1,22 +1,38 @@
 from functools import wraps
 import asyncio
-
+from concurrent.futures import TimeoutError
 from .log import logger
 
-def retry(should_retry_for_result, should_retry_for_exception, multiplier=2):
+
+def _default_retry_for_result(result):
+    return False
+
+
+def _default_retry_for_exception(exception):
+    return False
+
+
+def retry(retry_for_result=_default_retry_for_result, retry_for_exception=_default_retry_for_exception, multiplier=2,
+          timeout=None):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            yield from RetryHandler(should_retry_for_result, should_retry_for_exception, multiplier).run(func, *args,
-                                                                                                         **kwargs)
+            yield from RetryHandler(retry_for_result, retry_for_exception, multiplier, timeout).run(func, *args,
+                                                                                                    **kwargs)
+
         return wrapper
+
     return decorator
 
+
 class RetryHandler:
-    def __init__(self, should_retry_for_result, should_retry_for_exception, multiplier=2):
+    def __init__(self, should_retry_for_result=_default_retry_for_result,
+                 should_retry_for_exception=_default_retry_for_exception,
+                 multiplier=2, timeout=None):
         self._should_retry_for_result = should_retry_for_result
         self._should_retry_for_exception = should_retry_for_exception
         self._multiplier = multiplier
+        self._timeout = timeout
 
     @asyncio.coroutine
     def run(self, func, *args, **kwargs):
@@ -31,12 +47,19 @@ class RetryHandler:
         logger.info('Retrying %s after %s seconds', func.__name__, wait_time)
         yield from asyncio.sleep(wait_time)
         try:
-            result = yield from func(*args, **kwargs)
+            if self._timeout is None:
+                result = yield from func(*args, **kwargs)
+            else:
+                try:
+                    result = yield from asyncio.wait_for(func(*args, **kwargs), self._timeout)
+                except TimeoutError:
+                    yield from self._run_task(attempts_made + 1, func, *args, **kwargs)
+                    return
             logger.info('Result retrieved from %s is %s', func.__name__, result)
             if self._should_retry_for_result(result):
                 yield from self._run_task(attempts_made + 1, func, *args, **kwargs)
 
         except Exception as e:
-            logger.info('Debugging %s because it raised exception %s', func.__name__, str(e))
+            logger.info('%s raised exception %s', func.__name__, e.__class__.__name__)
             if self._should_retry_for_exception(e):
                 yield from self._run_task(attempts_made + 1, func, *args, **kwargs)
