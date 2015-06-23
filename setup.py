589d243747cbedab9ef572fd71da244a51a7ec0f
Ankit Chandawala <ankitchandawala@mgail.com>
2015-06-23 14:05:30 +0530

    Adding setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='async_retrial',
+      version='0.0.0',
+      author='Ankit Chandawala',
+      author_email='ankitchandawala@gmail.com',
+      url='https://github.com/nerandell/async_retrial',
+      description='asyncio retrial library',
+      packages=['retrial.retrial'])
+
