57e659c55c72a206f69698a51032ed1f58301996
nerandell <ankitchandawala@gmail.com>
2015-10-19 10:25:50 +0530

    Bump up version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='async_retrial',
-      version='0.4',
+      version='0.5',
       author='Ankit Chandawala',
       author_email='ankitchandawala@gmail.com',
       url='https://github.com/nerandell/async_retrial',
