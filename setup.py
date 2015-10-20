3ad50598abc4332a57558a16a58167f3d86ad0a3
nerandell <ankitchandawala@gmail.com>
2015-10-20 15:40:27 +0530

    Bumping up verion number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='async_retrial',
-      version='0.6',
+      version='0.7',
       author='Ankit Chandawala',
       author_email='ankitchandawala@gmail.com',
       url='https://github.com/nerandell/async_retrial',
