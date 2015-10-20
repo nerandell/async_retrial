c7fa96473f769cfcb9ec278d1d6d57b4d09948f7
nerandell <ankitchandawala@gmail.com>
2015-10-20 15:32:13 +0530

    Update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='async_retrial',
-      version='0.5',
+      version='0.6',
       author='Ankit Chandawala',
       author_email='ankitchandawala@gmail.com',
       url='https://github.com/nerandell/async_retrial',
